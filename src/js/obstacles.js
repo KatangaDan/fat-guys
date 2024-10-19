ef38bc6eb94b30445ef43226fe3b0b2359b6a6fa
Danish Saleem <saleemdf99@gmail.com>
2024-10-19 23:29:28 +0200

    .

--- src/js/obstacles.js
+++ src/js/obstacles.js
@@ -20,4 +20,19 @@ export function createPillar(world, scene, x, y, z, width, height, length) {
   const groundBody = new CANNON.Body({ mass: 0, shape: groundShape });
   groundBody.position.set(x, y, z + length / 2);
   world.addBody(groundBody);
+
+  //return the pillar position
+  return pillar.position;
 }
+
+// export function createGate(world, scene, x, y, z, width, height, length) {
+//   //X, Y, Z IS THE POSITION OF THE GROUND PIECE, STARTING FROM THE CENTER
+//   //Create a simple plane for the ground
+//   const gateGeometry = new THREE.BoxGeometry(width, height, length);
+//   const gateMaterial = new THREE.MeshStandardMaterial({ color: "blue" });
+//   const gate = new THREE.Mesh(gateGeometry, gateMaterial);
+//   gate.position.set(x + width / 2, y + height / 2, z + length / 2);
+//   gate.castShadow = true;
+//   gate.receiveShadow = true;
+//   scene.add(gate);
+// }

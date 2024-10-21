a6927660257b633e57d98e686ac8f9c58e6d730a
Danish Saleem <saleemdf99@gmail.com>
2024-10-21 10:53:38 +0200

    .

--- src/js/obstacles.js
+++ src/js/obstacles.js
@@ -95,7 +95,7 @@ export function createFan(scene, x, y, z, radius, lengthOfFans) {
   const center = new THREE.Mesh(centerGeometry, centerMaterial);
 
   // Create the fan blades
-  const bladeGeometry = new THREE.CylinderGeometry(1, 1, lengthOfFans, 32);
+  const bladeGeometry = new THREE.CylinderGeometry(0.75, 0.75, lengthOfFans, 32);
   const bladeMaterial = new THREE.MeshStandardMaterial({ color: "yellow" });
 
   const fan = new THREE.Group();
